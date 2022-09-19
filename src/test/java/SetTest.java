03aec8637322b59746cd554a99ddb601445bac18
badwrong <daniel.heidenhain@gmail.com>
2022-09-19 17:58:30 -0400

    make wrapper work

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -1,13 +1,14 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class SetTest {
 
     @Test
     public void testIsEmpty() {
         Set set = new Set();
-        assertEquals(set.isEmpty(), true);
+        assertTrue(set.isEmpty());
     }
 
 }
